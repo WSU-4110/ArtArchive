f3fe075a8193bf4d0f2eee2ca757aee328ad33f8
Ashley Long <gh0483@wayne.edu>
2020-11-28 14:09:31 -0500

    adding classes to test the validity of user submissions

--- web/insert.php
+++ web/insert.php
@@ -13,6 +13,41 @@ $firstname = mysqli_real_escape_string($link, $_REQUEST['firstname']);
 $lastname = mysqli_real_escape_string($link, $_REQUEST['lastname']);
 $description = mysqli_real_escape_string($link, $_REQUEST['description']);
  
+ final class firstname{
+	 private $firstname;
+	 
+	 private function __construct(string $firstname)
+	 {
+		 $this->testIfFirstNameValid($firstname);
+		 $this->firstname = $firstname;
+	 }
+	 
+	 private function testIfFirstNameValid(string $firstname)
+	 {
+		$firstname = trim($firstname);
+		$firstname = stripslashes($firstname);
+		$firstname = htmlspecialchars($firstname);
+		return $firstname;
+	 }
+	}
+	
+	 final class lastname{
+	 private $lastname;
+	 
+	 private function __construct(string $lastname)
+	 {
+		 $this->testIfFirstNameValid($lastname);
+		 $this->lastname = $lastname;
+	 }
+	 
+	 private function testIfFirstNameValid(string $lastname)
+	 {
+		$lastname = trim($lastname);
+		$lastname = stripslashes($lastname);
+		$lastname = htmlspecialchars($lastname);
+		return $lastname;
+	 }
+	}
 // Attempt insert query execution
 $sql = "INSERT INTO profileinfo (firstname, lastname, description) VALUES ('$firstname', '$lastname', '$description')";
 if(mysqli_query($link, $sql)){
