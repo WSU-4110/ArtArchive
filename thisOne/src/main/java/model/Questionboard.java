54e00a4fe02ce23b900cd3297d82e0f38ac9305e
SpaceMarine2040 <spartaman2040@gmail.com>
2020-12-02 18:53:37 -0500

    backup questions

---
+++ thisOne/src/main/java/model/Questionboard.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Questionboard {
+    public Questionboard(int topic_id, String topic_author, String topic_title, String topicquestion) {
+    }
+
+    public String getTopic_author() {
+    }
+
+    public String getTopic_title() {
+    }
+
+    public String getTopicquestion() {
+    }
+
+    public int getTopic_id() {
+    }
+}
